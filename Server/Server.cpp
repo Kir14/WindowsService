8e1606de28dd92e910c93f0bd195b5bbc311d36b
Kir14 <62447478+Kir14@users.noreply.github.com>
2020-06-12 19:39:56 +0300

    Server.cpp with deque

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,55 +2,52 @@
 #include <WinSock2.h>
 #include <iostream>
 #include <WS2tcpip.h>
-#include <vector>
+
+#include <deque>
 
 
 
 SOCKET Connect;
 //SOCKET *Connections;
-std::vector<SOCKET> Connections;
+std::deque<SOCKET> Connections;
 SOCKET Listen;
 
 
 int ClientCount = 0;
 int ClientMax = 100;
 
-void SendMessageToClient(int ID)
+void SendMessageToClient(SOCKET Client)
 {
 	char buffer[1024];
 	for (;; Sleep(50))
 	{
-		try
+
+		
+
+		memset(buffer, 0, sizeof(buffer));
+		
+		if (recv(*std::find(Connections.begin(), Connections.end(), Client), buffer, 1024, NULL) != SOCKET_ERROR)
 		{
-
-			if (Connections[ID] == NULL)
+			if (errno) return;
+			printf("\n");
+			printf(buffer);
+			for (SOCKET sk : Connections)
 			{
-				break;
-			}
-			memset(buffer, 0, sizeof(buffer));
-			if (recv(Connections[ID], buffer, 1024, NULL) !=  SOCKET_ERROR)
-			{	
-				if (errno) return;
-				printf("\n");
-				printf(buffer);
-				for (SOCKET sk : Connections)
-				{
-					send(sk, buffer, strlen(buffer), NULL);
+				send(sk, buffer, strlen(buffer), NULL);
 
-				}
-			}
-			else
-			{
-				ClientCount--;
-				Connections.erase(Connections.begin() + ID);
-				printf("Disconnect");
-				return ;
 			}
 		}
-		catch(_exception exp)
+		else
 		{
-
+			ClientCount--;
+			closesocket(*std::find(Connections.begin(), Connections.end(), Client));
+			Connections.erase(std::find(Connections.begin(), Connections.end(), Client));
+			Connections.shrink_to_fit();
+			printf("\n");
+			printf("Disconnect");
+			return;
 		}
+
 	}
 	//delete []buffer;
 }
@@ -61,22 +58,22 @@ int main()
 	HANDLE thr;
 	WSADATA data;
 	WORD version = MAKEWORD(2, 2);
-	int res = WSAStartup(version, &data); 
+	int res = WSAStartup(version, &data);
 	if (res != 0)
 	{
 		return 1;
 	}
 	/*   можно заменить
 	if (FAILED (WSAStartup (MAKEWORD( 2, 2 ), &data) ) )
-    {
+	{
 	  // Error...
 	  error = WSAGetLastError();
 	  //...
-    }
+	}
 	*/
 
 	//Connections = new SOCKET[ClientMax];
-	
+
 	// С сайта https://club.shelek.ru/viewart.php?id=35
 	sockaddr_in SAddr;
 	ZeroMemory(&SAddr, sizeof(SAddr));
@@ -108,18 +105,21 @@ int main()
 
 	listen(Listen, ClientMax);
 	printf("Start server...");
-	
-	while(true)
+
+	SOCKET Test;
+	Connections.push_back(Test);
+	while (true)
 	{
 		Sleep(50);
 		if (Connect = accept(Listen, NULL, NULL))
 		{
+			printf("\n");
 			printf("Client connect");
 			//Connections[ClientCount] = Connect;
 			Connections.push_back(Connect);
-			thr=CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SendMessageToClient, (LPVOID)ClientCount, NULL, NULL);
+			thr = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SendMessageToClient, (LPVOID)Connect, NULL, NULL);
 			ClientCount++;
-			
+
 		}
 
 	}
